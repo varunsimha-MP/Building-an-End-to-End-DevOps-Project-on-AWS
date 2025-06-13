4e5d713f53cf2559af1e95746f206960f2f00541
varun <varun@gmail.com>
2025-06-13 04:28:33 +0000

    added ec2 in terraform

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -2,6 +2,27 @@ module "vpc" {
   source = "./vpc"
 }
 
+module "ec2" {
+  source = "./ec2"
+  vpc_id = module.vpc.vpc_id
+  subnet_id = module.vpc.subnet_ids
+  key = "singapure-key"
+  ec2_ingress_rule = {
+    "all" = {
+      port = 0
+      protocol = "-1"
+      cidr_block = ["0.0.0.0/0"]
+      description = "Allowing All Traffic"
+    }
+    "SSH" = {
+      port = 22
+      protocol = "tcp"
+      cidr_block = ["0.0.0.0/0"]
+      description = "Allowing SSH traffic"
+    }
+  }
+}
+
 module "eks" {
   source = "./eks"
   vpc_id = module.vpc.vpc_id
